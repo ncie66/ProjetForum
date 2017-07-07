2f74174ebb968c024ea541d4932417c12e91a5fe
yohann <ym66300@gmail.com>
2017-07-07 15:04:08 +0200

    maj

--- fichier_01.php
+++ fichier_01.php
@@ -1,1 +1,24 @@
-yohann
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet</title>
+</head>
+<body>
+   <form method="post" action="service.php">
+            <label>username </label>
+            <input  type="text" name="nom"><br><br>
+
+            <label>password </label>
+            <input  type="password" name="password" ><br><br>
+
+             <label>email </label>
+            <input  type="text" name="mail"><br><br>
+
+        <input type="submit" class="Envoyer" value="Ajouter">
+        
+    </form>
+</body>
+</html>
